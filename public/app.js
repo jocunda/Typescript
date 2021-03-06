2426c6ebba3141d57aaa3c5dd4ebbba4b82fa912
jocunda <silviawanshan88@gmail.com>
2021-03-06 17:42:13 +0800

    sandbox delete

--- public/app.js
+++ public/app.js
@@ -1,15 +1,37 @@
 "use strict";
+class Invoice {
+    // readonly client: string;
+    // private details: string;
+    // public amount: number;
+    constructor(client, details, amount) {
+        this.client = client;
+        this.details = details;
+        this.amount = amount;
+    }
+    format() {
+        return `${this.client} owes Rp${this.amount} for ${this.details}`;
+    }
+}
+const invOne = new Invoice('mario', 'work on mario website', 250);
+const invTwo = new Invoice('luigi', 'work on luigi website', 300);
+let invoices = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+invoices.forEach(inv => {
+    inv.client = 'something else';
+    console.log(inv.client, inv.amount, inv.format());
+});
 // const anchor = document.querySelector('a')!;
 // console.log(anchor.href);
 // const form = document.querySelector('form')!;
-var form = document.querySelector('.new-item-form');
+const form = document.querySelector('.new-item-form');
 // console.log(form.children);
 // inputs
-var type = document.querySelector('#type');
-var tofrom = document.querySelector('#tofrom');
-var details = document.querySelector('#details');
-var amount = document.querySelector('#amount');
-form.addEventListener('submit', function (e) {
+const type = document.querySelector('#type');
+const tofrom = document.querySelector('#tofrom');
+const details = document.querySelector('#details');
+const amount = document.querySelector('#amount');
+form.addEventListener('submit', (e) => {
     e.preventDefault();
     console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
 });
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,38 @@
+class Invoice{
+    // readonly client: string;
+    // private details: string;
+    // public amount: number;
+
+    constructor(
+        readonly client: string,
+        private details: string,
+        public amount: number,
+    ) { }
+        
+    
+    format() {
+        
+        return `${this.client} owes Rp${this.amount} for ${this.details}`;
+    }
+}
+
+const invOne = new Invoice('mario', 'work on mario website', 250);
+const invTwo = new Invoice('luigi', 'work on luigi website', 300);
+
+let invoices: Invoice[] = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+
+invoices.forEach(inv => {
+    inv.client = 'something else';
+    console.log(inv.client, inv.amount, inv.format());
+});
+
+
+
+
+
+
 // const anchor = document.querySelector('a')!;
 
 // console.log(anchor.href);
